32b40b9c453ad4b4e3e9771bfcfaff6fbcb2ae9c
Job Nyaribari <jobnyaribari808@gmail.com>
2025-12-14 09:36:17 +0300

    settings.py file changed

--- AgentTrader/settings.py
+++ AgentTrader/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default="unsafe-dev-secret-key")
 GROQ_API_KEY = config('GROQ_API_KEY')
 TWELVE_DATA_API_KEY = config('TWELVE_DATA_API_KEY')
 
@@ -47,13 +47,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+ 
 ]
 
 ROOT_URLCONF = 'AgentTrader.urls'
